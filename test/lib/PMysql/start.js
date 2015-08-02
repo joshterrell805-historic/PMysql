cda4efa2d758cef97d46c8cd23a1b72cdaa9820d
Josh Terrell <joshterrell805@gmail.com>
2015-08-02 04:23:27 -0700

    start: convert to mocha

--- test/lib/PMysql/start.js
+++ test/lib/PMysql/start.js
@@ -1,29 +1,26 @@
-var vows = require('vows'),
-    assert = require('assert');
+var assert = require('assert');
 
 var PMysql = require('lib/PMysql');
 
-var suite = vows.describe('PMysql');
+describe('start', function() {
+  it('should create the connection pool', function() {
+    var config = null;
+    var pMysql = new PMysql('the config', {
+      createPool: function(conf) {
+        config = conf;
+        return 23;
+      },
+    });
 
-suite.addBatch({
-  'start': {
-    'initialize `this.pool`': function() {
-      var config = null;
-      var pMysql = new PMysql('the config', {
-        createPool: function(conf) {
-          config = conf;
-          return 23;
-        },
-      });
+    // before calling
+    assert.strictEqual(pMysql.pool, null);
 
-      assert.strictEqual(pMysql.pool, null);
+    pMysql.start();
 
-      pMysql.start();
-
-      assert.strictEqual(pMysql.pool, 23);
-      assert.strictEqual(config, 'the config');
-    },
-  }
+    // the pool is equal to what createPool returned
+    assert.strictEqual(pMysql.pool, 23);
+    // the config passed to createPool is whatever the user passed to the
+    // constructor of PMysql
+    assert.strictEqual(config, 'the config');
+  });
 });
-
-suite.export(module);
