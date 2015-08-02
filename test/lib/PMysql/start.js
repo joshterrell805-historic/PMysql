2019938bfa5e4ced4263057ee342d07fa69e5a66
Josh Terrell <joshterrell805@gmail.com>
2015-08-02 06:27:03 -0700

    PMysql#start only allow starting once

--- test/lib/PMysql/start.js
+++ test/lib/PMysql/start.js
@@ -4,21 +4,41 @@ var assert = require('assert'),
 var PMysql = require('lib/PMysql');
 
 describe('start', function() {
-  var createPoolRetval;
-  var self = {
-    config: 72,
-    mysql: {
-      createPool: sinon.spy(function() {return createPoolRetval;}),
-    },
-    pool: null,
-  };
+  var self, createPoolRetval;
+
+  beforeEach(function() {
+    createPoolRetval = null;
+    self = {
+      config: null,
+      mysql: {
+        createPool: sinon.spy(function() {return createPoolRetval;}),
+      },
+      pool: null,
+    };
+  });
 
   it('should create the connection pool', function() {
     createPoolRetval = 71;
+    self.config = {asdf: 'zzz'};
 
     PMysql.prototype.start.call(self);
 
     assert.strictEqual(self.pool, createPoolRetval);
     assert(self.mysql.createPool.calledWith(self.config));
+  });
+
+  it('should only allow starting once', function() {
+    createPoolRetval = {};
+    PMysql.prototype.start.call(self);
+
+    assert(self.mysql.createPool.callCount, 1);
+
+    try {
+      PMysql.prototype.start.call(self);
+      assert(false);
+    } catch (e) {
+      assert.strictEqual(e.message, 'this PMysql instance is already started');
+      assert(self.mysql.createPool.callCount, 1);
+    }
   });
 });
