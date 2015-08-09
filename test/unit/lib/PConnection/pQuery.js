5791b2b47a220e0e5e72d9965d081562fc1e8212
Josh Terrell <joshterrell805@gmail.com>
2015-08-09 15:16:25 -0700

    (pConnection) for chaining, don't pass undefined values as the last parameter

--- test/unit/lib/PConnection/pQuery.js
+++ test/unit/lib/PConnection/pQuery.js
@@ -59,7 +59,23 @@ describe('pQuery', function() {
   it('should pass arguments to `this.connection.query`', function() {
     return PConnection.prototype.pQuery.call(self, 7, 4, 3, 4, 5)
     .then(function() {
-      assert(self.connection.query.calledWith(7, 4, 3, 4, 5));
+      assert.strictEqual(self.connection.query.callCount, 1);
+      assert.strictEqual(self.connection.query.args[0].length, 6);
+      assert.deepEqual(self.connection.query.args[0].splice(0,
+          self.connection.query.args[0].length - 1), [7, 4, 3, 4, 5]);
+      assert(self.connection.query.args[0][0] instanceof Function);
+    });
+  });
+
+  it('should not pass last argument if it is undefined (chaining)', function() {
+    return PConnection.prototype.pQuery.call(self, 1, undefined).return()
+    .then(PConnection.prototype.pQuery.bind(self, 2))
+    .then(function() {
+      assert.strictEqual(self.connection.query.callCount, 2);
+      assert.strictEqual(self.connection.query.args[0].length, 2);
+      assert.strictEqual(self.connection.query.args[0][0], 1);
+      assert.strictEqual(self.connection.query.args[1].length, 2);
+      assert.strictEqual(self.connection.query.args[1][0], 2);
     });
   });
 });
